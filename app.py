cf87378f280bd496829b7a1fd6a3e6abd12b3ced
bruno <bruno.brito.ln@gmail.com>
2018-03-06 15:26:02 -0300

    refactor

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ class Discipline:
         }
 
 
-# Cookie: _ga=GA1.2.406708436.1498888342; _gid=GA1.2.184563253.1498888342; _icl_visitor_lang_js=en-us; _mkto_trk=id:398-IBA-570&token:_mch-voxy.com-1498888349177-98776; my_language=en; sessionid=3rsoz2jf3i3n47egzbbv2fypyziwt3f8; SnapABugRef=https%3A%2F%2Fapp.voxy.com%2Fgo%2Flogin%20https%3A%2F%2Fvoxy.com%2F; SnapABugHistory=1#; SnapABugVisit=1#1498888355; SnapABugChatWindow=%7C0%7C-1%2C0%2C-1%2C0; __utma=173626660.406708436.1498888342.1498888356.1498888356.1; __utmb=173626660.1.10.1498888356; __utmc=173626660; __utmz=173626660.1498888356.1.1.utmcsr=voxy.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmt=1
-
-s = requests.session()
-
-
-# s.cookies()
+class Url:
+    LOGIN_URL = 'http://187.19.195.156/corpore.net/Login.aspx'
+    CONTEXT_URL = 'http://187.19.195.156/Corpore.Net/Source/Edu-Educacional/RM.EDU.CONTEXTO/EduSelecionarContextoModalWebForm.aspx?Qs=SelectedMenuIDKey%3dMainEducacional'
+    SCORE_URL = 'http://187.19.195.156/Corpore.Net/Source/Edu-Educacional/RM.EDU.CONTEXTO/EduSelecionarContextoModalWebForm.aspx?Qs=ActionID%3dEduNotaEtapaActionWeb%26SelectedMenuIDKey%3dmnNotasEtapa'
+    FAULT_URL = 'http://187.19.195.156/Corpore.Net/Main.aspx?ActionID=EduQuadroAvisoActionWeb&SelectedMenuIDKey=mnQuadroAviso'
+    SCORE = 'http://187.19.195.156/Corpore.Net/Main.aspx?ActionID=EduNotaEtapaActionWeb&SelectedMenuIDKey=mnNotasEtapa'
 
 
 def get_hidden_fields(response):
@@ -36,17 +36,10 @@ def get_hidden_fields(response):
     return form
 
 
-def do_all_stuff(username, password):
-    LOGIN_URL = 'http://187.19.195.156/corpore.net/Login.aspx'
-    CONTEXT_URL = 'http://187.19.195.156/Corpore.Net/Source/Edu-Educacional/RM.EDU.CONTEXTO/EduSelecionarContextoModalWebForm.aspx?Qs=SelectedMenuIDKey%3dMainEducacional'
-    SCORE_URL = 'http://187.19.195.156/Corpore.Net/Source/Edu-Educacional/RM.EDU.CONTEXTO/EduSelecionarContextoModalWebForm.aspx?Qs=ActionID%3dEduNotaEtapaActionWeb%26SelectedMenuIDKey%3dmnNotasEtapa'
-    FAULT_URL = 'http://187.19.195.156/Corpore.Net/Main.aspx?ActionID=EduQuadroAvisoActionWeb&SelectedMenuIDKey=mnQuadroAviso'
-
-    disciplines = []
-
+def login(username, password):
     session = requests.session()
 
-    login = session.get(LOGIN_URL)
+    login = session.get(Url.LOGIN_URL)
 
     form = get_hidden_fields(login)
 
@@ -55,48 +48,49 @@ def do_all_stuff(username, password):
     form["ddlAlias"] = "CorporeRM"
     form["btnLogin"] = "Acessar"
 
-    session.post(LOGIN_URL, data=form)
+    session.post(Url.LOGIN_URL, data=form)
 
-    response = session.get(SCORE_URL)
+    response = session.get(Url.SCORE_URL)
 
+    return session, response
+
+
+def get_page(response, session, url, context_number=-1):
     soup = BeautifulSoup(response.text, "lxml")
     inputs = soup.find_all(id="rdContexto")
-    if len(inputs) == 2:
-        rdContexto = inputs[1]["value"]
-    if len(inputs) == 3:
-        rdContexto = inputs[2]["value"]
-    else:
-        rdContexto = inputs[0]["value"]
 
-    form = get_hidden_fields(response)
-    form["rdContexto"] = rdContexto
+    rd_context = inputs[context_number]['value']
 
-    session.post(CONTEXT_URL, data=form)
+    if rd_context:
+        form = get_hidden_fields(response)
+        form["rdContexto"] = rd_context
 
-    response = session.get(
-        "http://187.19.195.156/Corpore.Net/Main.aspx?ActionID=EduNotaEtapaActionWeb&SelectedMenuIDKey=mnNotasEtapa")
+        session.post(Url.CONTEXT_URL, data=form)
+
+        response = session.get(url)
+        return session, response
+
+
+class Context:
+    def __init__(self, session, response):
+        self.session = session
+        self.response = response
+
+
+def get_disciplines(response):
+    disciplines = []
 
     soup = BeautifulSoup(response.text, "lxml")
     table = soup.find("table", {"id": "ctl24_xgvNotasFilial_DXMainTable"})
 
     rows = table.findChildren(['th', 'tr'])
 
-    table_faults = soup.find("table", {"id": "ctl24_xgvFaltasFilial_DXMainTable"})
-
-    rows_faults = table_faults.findChildren(['th', 'tr'])
-
-
-
-    name = []
-    status = []
-    score = []
-
     for row in rows[12:]:
         name = row.findChildren('td')[3].text
         status = row.findChildren('td')[4].text
         score = row.findChildren('td')[5].text, row.findChildren('td')[6].text, row.findChildren('td')[7].text, \
                 row.findChildren('td')[8].text, row.findChildren('td')[9].text
-        disciplines.append(Discipline(name, status, score))
+        yield Discipline(name, status, score)
 
     return disciplines
 
@@ -114,10 +108,10 @@ def get_score():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # username = "201519019"
-    # password = "System.out"
+    session, response = login(username, password)
+    session, response = get_page(response, session, Url.SCORE)
 
-    return render_template("score.html", disciplines=do_all_stuff(username, password))
+    return render_template("score.html", disciplines=get_disciplines(response))
     # disciplines = do_all_stuff(username, password)
     # return jsonify([dis.serialize() for dis in disciplines])
 
